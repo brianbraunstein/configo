90968b25400fc72577439f2226a9d633e1ad07d2
Brian Braunstein <git@bristyle.com>
2023-06-13 11:23:52 +0200

    Add IPv6 testing to cidrSubnet tests.

--- lib/util_test.go
+++ lib/util_test.go
@@ -22,12 +22,23 @@ func (tc *testCase) Run(t *testing.T) {
 
 func TestCidrSubnet(t *testing.T) {
   testCases := []testCase{
+
+    // IPv4
     {cidr: "10.0.0.3/16", subnetBits: 4, subnetNumber: 3,
      expected: "10.0.48.0/20"},
     {cidr: "10.0.0.3/16", subnetBits: 0, subnetNumber: 0,
      expected: "10.0.0.0/16"},
     {cidr: "10.0.0.3/16", subnetBits: 16, subnetNumber: 1 << 15 + 7,
      expected: "10.0.128.7/32"},
+
+    // IPv6
+    {cidr: "fc00::/8", subnetBits: 8, subnetNumber: 1 << 6 + 3,
+     expected: "fc43::/16"},
+    {cidr: "fc00::/28", subnetBits: 36, subnetNumber: 0xabcd << 20 + 7,
+     expected: "fc00:a:bcd0:7::/64"},
+    {cidr: "fc00::/30", subnetBits: 0, subnetNumber: 0,
+     expected: "fc00::/30"},
+     
   }
   for _, tc := range testCases { tc.Run(t) }
 }
